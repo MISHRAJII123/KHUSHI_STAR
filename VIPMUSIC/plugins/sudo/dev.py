202316ba6a9a964bc808d5270cbbe83fe6f0b49e
MISHRAJII123 <ccaddnew@gmail.com>
2024-12-05 15:27:57 +0530

    Update dev.py

--- VIPMUSIC/plugins/sudo/dev.py
+++ VIPMUSIC/plugins/sudo/dev.py
@@ -213,6 +213,32 @@ async def shellrunner(_, message: Message):
 
     await message.stop_propagation()
 
+app.on_message(filters.command(["restart"]) & OWNER_ID)
+async def restart_(_, message):
+    response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
+    ac_chats = await get_active_chats()
+    for x in ac_chats:
+        try:
+            await app.send_message(
+                chat_id=int(x),
+                text=f"{app.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
+            )
+            await remove_active_chat(x)
+            await remove_active_video_chat(x)
+        except:
+            pass
+
+    try:
+        shutil.rmtree("downloads")
+        shutil.rmtree("raw_files")
+        shutil.rmtree("cache")
+    except:
+        pass
+    await response.edit_text(
+        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
+    )
+    os.system(f"kill -9 {os.getpid()} && python3 -m VIPMUSIC")
+
 
 __MODULE__ = "Deᴠ"
 __HELP__ = """
